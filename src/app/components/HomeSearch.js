3fb67de100a87fe77d0de37a77e3ff8188a2e872
DeveloperYasmin <banuyasmin96@gmail.com>
2024-01-26 12:05:40 +0530

    Lucky

--- src/app/components/HomeSearch.js
+++ src/app/components/HomeSearch.js
@@ -9,11 +9,19 @@ import { useRouter } from 'next/navigation';
 const HomeSearch = () => {
     const router=useRouter()
     const [input,setinput]=useState("")
+    const[randomsearchloading,setrandomsearchloading]=useState(false)
     const handlesubmit=(e)=>{
         e.preventDefault()
         if(!input.trim()) return
         router.push(`/search/web?searchTerm=${input}`)
        }
+       const randomsearch=async()=>{
+        setrandomsearchloading(true)
+        const response=await fetch("https://random-word-api.herokuapp.com/word").then((res)=>res.json()).then((data)=>data[0])
+      if(!response)    return
+      router.push(`/search/web?searchTerm=${response}`)
+      setrandomsearchloading(false)
+    }
   return ( <>
    <form onSubmit={handlesubmit} className='flex sm:max-w-xl lg:max-w-xl focus-within:shadow-md w-full mt-5 mx-auto max-w-[90%] border rounded-full border-gray-200 px-5 py-3'>
     <IoIosSearch className='text-xl hover:shadow-md transition-shadow text-gray-800'/>
@@ -22,7 +30,9 @@ const HomeSearch = () => {
    </form>
    <div className='flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8'>
     <button onClick={handlesubmit} className='btn'>Google Search</button>
-    <button className='btn'>I'm Feeling Lucky</button>
+    <button disabled={randomsearchloading} onClick={randomsearch} className='btn flex items-center justify-center disabled:opacity-80'>
+      {randomsearchloading?(<img src="spin.svg" alt="spin" className='h-6 text-center'/>):"I'm Feeling Lucky"}
+      </button>
 
 
    </div></>
