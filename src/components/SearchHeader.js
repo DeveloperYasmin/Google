e1ffe6b7189bbbef75ac4730c4be804efc87bd48
DeveloperYasmin <banuyasmin96@gmail.com>
2024-01-27 08:17:51 +0530

    Search

--- src/components/SearchHeader.js
+++ src/components/SearchHeader.js
@@ -1,8 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
+import Searchbox from './Searchbox'
+import { RiSettings3Line } from "react-icons/ri";
+import { CgMenuGridO } from "react-icons/cg";
+import SearchHeaderOptions from './SearchHeaderOptions';
+
 
 const SearchHeader = () => {
   return (
-    <div>SearchHeader</div>
+   <header className='sticky top-0 bg-white bg-opacity-10'>
+    <div className='flex w-full p-6 items-center justify-between'>
+      <Link href={"/"}>
+      <h1 className='text-5xl font-extrabold text-black animate-pulse '>Google</h1>
+      </Link>
+      <div className='flex-1'>
+        <Searchbox/>
+        </div>
+        <div className='hidden md:inline-flex space-x-2 '>
+        <RiSettings3Line className='header-icon' />
+        <CgMenuGridO className='header-icon' />
+        </div>
+        <button className='bg-gradient-to-r from-blue-600 to-red-400 transition-shadow text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md ml-2'>Sign in
+        </button>
+    </div>
+  <SearchHeaderOptions/>
+   </header>
   )
 }
 
