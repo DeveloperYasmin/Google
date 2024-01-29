ba30b8b1e0571b0cf34c0ca9da9e28ea73051550
DeveloperYasmin <banuyasmin96@gmail.com>
2024-01-29 08:36:05 +0530

    updated

--- src/components/ImageSearchResults.js
+++ src/components/ImageSearchResults.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import PaginationButtons from './PaginationButtons'
+import Image from 'next/image'
 
 const ImageSearchResults = ({results}) => {
   return (
