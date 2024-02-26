cce3ab6dc7967508269bab9fff187afd97463bfa
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-26 20:48:03 +0530

    now

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,32 +1,45 @@
 import WebSearchResult from '@/components/WebSearchResult'
 import Link from 'next/link'
 import React, { Suspense } from 'react'
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/router'
 
+const WebSearchPage = () => {
+  const searchParams = useSearchParams();
+  const startIndex = searchParams.start || "1"
 
-const WebSearchPage =async ({searchParams}) => {
-
-  await new Promise((resolve)=>setTimeout(resolve,7000))
-
-  const startIndex=searchParams.start || "1"
-  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&start=${startIndex}`)
-  if(!response.ok){
-    throw new Error("Something went wrong")
+  const fetchData = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 7000))
+    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`)
+    if (!response.ok) {
+      throw new Error("Something went wrong")
+    }
+    const data = await response.json()
+    return data;
   }
-   const data= await response.json()
-   const results=data.items;
-   if(!results){
-    return( <div className=' flex flex-col mt-60 justify-center items-center pt-10'>
-      <h1 className='text-3xl mb-4'>No results found:(</h1>
-      <p className='text-lg'>Try search something else or go back to {" "}
-      <Link href="/" className="text-blue-500">homepage</Link>
-      </p>
-    </div>)
-   }
 
-  return <Suspense>
-  {results && <WebSearchResult results={data}/>}
-  </Suspense>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WebDataFetcher fetchData={fetchData} />
+    </Suspense>
+  );
 }
 
-export default WebSearchPage
+const WebDataFetcher = ({ fetchData }) => {
+  const data = fetchData();
+  const results = data?.items;
+
+  if (!results) {
+    return (
+      <div className=' flex flex-col mt-60 justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>No results found:(</h1>
+        <p className='text-lg'>Try searching for something else or go back to {" "}
+          <Link href="/" className="text-blue-500">homepage</Link>
+        </p>
+      </div>
+    );
+  }
+
+  return <WebSearchResult results={results} />;
+}
+
+export default WebSearchPage;
