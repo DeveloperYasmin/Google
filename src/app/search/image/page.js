38c29027572065d695692c266e88934458d85493
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-27 13:27:45 +0530

    today

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,30 +1,32 @@
 import ImageSearchResults from '@/components/ImageSearchResults'
 import Link from 'next/link'
-
 import React, { Suspense } from 'react'
 
-const ImageSearchPage =async ({searchParams}) => {
-    const searchTerm=searchParams.get("searchTerm")
-    const startIndex=searchParams.get("start")||1
-  await new Promise((resolve)=>setTimeout(resolve,7000))
-  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}}&searchType=image&start=${startIndex}`)
-  if(!response.ok){
-    throw new Error("Something went wrong")
+const ImageSearchPage = ({ searchParams }) => {
+  // Extract search term and start index from searchParams
+  const searchTerm = searchParams.get("searchTerm")
+  const startIndex = searchParams.get("start") || "1"
+
+  // If the search term is not provided, return an error message
+  if (!searchTerm) {
+    return (
+      <div className='flex flex-col mt-60 justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>No search term provided :(</h1>
+        <p className='text-lg'>
+          Please go back to <Link href="/" className="text-blue-500">homepage</Link> and provide a search term.
+        </p>
+      </div>
+    )
   }
-   const data= await response.json()
-   const results=data.items;
-   if(!results){
-    return( <div className=' flex flex-col mt-60 justify-center items-center pt-10'>
-      <h1 className='text-3xl mb-4'>No results found:(</h1>
-      <p className='text-lg'>Try search something else or go back to {" "}
-      <Link href="/" className="text-blue-500">homepage</Link>
-      </p>
-    </div>)
-   }
 
-  return <> <Suspense>  {results && <ImageSearchResults results={data}/>} </Suspense>
-
-  </>;
+  return (
+    <>
+      {/* Render search results */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <ImageSearchResults searchTerm={searchTerm} startIndex={startIndex} />
+      </Suspense>
+    </>
+  )
 }
 
 export default ImageSearchPage
