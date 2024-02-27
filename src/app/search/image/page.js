d9a99b8ab0ce1b849d89af11b2a654e8b1ba3a46
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-27 12:25:14 +0530

    today

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -19,19 +19,20 @@ const ImageSearchPage = async () => {
    const results=data.items;
    if(!results){
     return( 
-    <Suspense>
+      <Suspense>
     <div className=' flex flex-col mt-60 justify-center items-center pt-10'><Link href={`${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}}&searchType=image&start=${startIndex}`}></Link>
       <h1 className='text-3xl mb-4'>No results found:(</h1>
       <p className='text-lg'>Try search something else or go back to {" "}
       <Link href="/" className="text-blue-500">homepage</Link>
       </p>
     </div>
-    </Suspense>)
-   
-  }
-  return <>
-  {results && <ImageSearchResults results={data}/>}
-  </>;
+    </Suspense>)}
+    
+    { <Suspense>
+      results && <ImageSearchResults results={data}/>
+      </Suspense>}
+  
+
 }
 
 export default ImageSearchPage
