469599aef821053378a3c2e992c8ab0ec5bb7ffd
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-27 13:00:32 +0530

    today

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,29 +1,25 @@
-import ImageSearchResults from '@/components/ImageSearchResults';
-import Link from 'next/link';
-import React, { Suspense } from 'react';
-import { useRouter } from 'next/navigation';
+import ImageSearchResults from '@/components/ImageSearchResults'
+import Link from 'next/link'
+import React, { Suspense } from 'react'
 
-const ImageSearchPage = () => {
-  const router = useRouter();
-  const { searchParams } = router.query;
-  const startIndex = searchParams?.start || "1";
-
+const ImageSearchPage = ({ searchParams }) => {
+  const startIndex = searchParams.start || "1"
   const fetchData = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 7000));
-    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`);
+    await new Promise((resolve) => setTimeout(resolve, 7000))
+    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`)
     if (!response.ok) {
-      throw new Error("Something went wrong");
+      throw new Error("Something went wrong")
     }
-    const data = await response.json();
+    const data = await response.json()
     return data;
-  };
+  }
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <ImageDataFetcher fetchData={fetchData} />
     </Suspense>
   );
-};
+}
 
 const ImageDataFetcher = ({ fetchData }) => {
   const data = fetchData();
@@ -31,7 +27,7 @@ const ImageDataFetcher = ({ fetchData }) => {
 
   if (!results) {
     return (
-      <div className='flex flex-col mt-60 justify-center items-center pt-10'>
+      <div className=' flex flex-col mt-60 justify-center items-center pt-10'>
         <h1 className='text-3xl mb-4'>No results found:(</h1>
         <p className='text-lg'>Try searching for something else or go back to {" "}
           <Link href="/" className="text-blue-500">homepage</Link>
@@ -41,6 +37,6 @@ const ImageDataFetcher = ({ fetchData }) => {
   }
 
   return <ImageSearchResults results={results} />;
-};
+}
 
 export default ImageSearchPage;
