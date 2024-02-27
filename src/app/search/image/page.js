809f3b9e2a15cf536c6759e25c495e331b14f212
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-27 12:18:05 +0530

    today

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -2,28 +2,33 @@ import ImageSearchResults from '@/components/ImageSearchResults'
 import {useSearchParams } from 'next/navigation'
 
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-const ImageSearchPage =async () => {
+const ImageSearchPage = async () => {
+
   const searchParams=useSearchParams()
   const searchTerm=searchParams.get("searchTerm")
-  const startIndex=searchParams.get||1
+  const startIndex=searchParams.get("start")||1
+  
   await new Promise((resolve)=>setTimeout(resolve,7000))
-  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}}&searchType=image&start=${startIndex}`)
+  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=`)
   if(!response.ok){
     throw new Error("Something went wrong")
   }
    const data= await response.json()
    const results=data.items;
    if(!results){
-    return( <div className=' flex flex-col mt-60 justify-center items-center pt-10'>
+    return( 
+    <Suspense>
+    <div className=' flex flex-col mt-60 justify-center items-center pt-10'><Link href={`${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}}&searchType=image&start=${startIndex}`}></Link>
       <h1 className='text-3xl mb-4'>No results found:(</h1>
       <p className='text-lg'>Try search something else or go back to {" "}
       <Link href="/" className="text-blue-500">homepage</Link>
       </p>
-    </div>)
-   }
-
+    </div>
+    </Suspense>)
+   
+  }
   return <>
   {results && <ImageSearchResults results={data}/>}
   </>;
