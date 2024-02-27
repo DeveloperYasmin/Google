7dde8d70db1820315f91943fd270150c05ec0926
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-27 12:35:24 +0530

    today

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,32 +1,9 @@
-
-
-import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation'
 import React from 'react'
-import { Suspense } from 'react'
 
-import { BsChevronLeft,BsChevronRight } from "react-icons/bs";
-
-const ImageSearchPage = () => {
-    const pathname=usePathname()
-    const searchParams=useSearchParams()
-    const searchTerm=searchParams.get("searchTerm")
-    const startIndex=+searchParams.get("start")||1
-    
+const page = () => {
   return (
-    <Suspense>
-    <div className='text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0'>
-       {startIndex>=10 && (<Link href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex-10}`}>
-       <div className='flex flex-col cursor-pointer items-center hover:underline'><BsChevronLeft className='h-5' />
-       <p>Previous</p></div>
-       </Link>)}
-       {startIndex<=90 && (<Link href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex+10}`}>
-       <div className='flex flex-col cursor-pointer items-center hover:underline'><BsChevronRight className='h-5' />
-       <p>Next</p></div>
-       </Link>)}
-    </div>
-    </Suspense>
+    <div>page</div>
   )
 }
 
-export default ImageSearchPage
+export default page
