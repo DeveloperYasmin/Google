9c99368b9b76e8940dae41addb0061ef9f577d38
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-27 13:14:30 +0530

    today

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,42 +1,32 @@
 import ImageSearchResults from '@/components/ImageSearchResults'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+
 import React, { Suspense } from 'react'
 
-const ImageSearchPage = ({ searchParams }) => {
-  const startIndex = searchParams.start || "1"
-  const fetchData = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 7000))
-    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`)
-    if (!response.ok) {
-      throw new Error("Something went wrong")
-    }
-    const data = await response.json()
-    return data;
+const ImageSearchPage =async () => {
+    const searchParams=useSearchParams()
+    const searchTerm=searchParams.get("searchTerm")
+    const startIndex=searchParams.get("start")||1
+  await new Promise((resolve)=>setTimeout(resolve,7000))
+  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=`)
+  if(!response.ok){
+    throw new Error("Something went wrong")
   }
+   const data= await response.json()
+   const results=data.items;
+   if(!results){
+    return(<Suspense> <div className=' flex flex-col mt-60 justify-center items-center pt-10'> <Link href={`${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}}&searchType=image&start=${startIndex}`}></Link>
+      <h1 className='text-3xl mb-4'>No results found:(</h1>
+      <p className='text-lg'>Try search something else or go back to {" "}
+      <Link href="/" className="text-blue-500">homepage</Link>
+      </p>
+    </div></Suspense>)
+   }
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ImageDataFetcher fetchData={fetchData} />
-    </Suspense>
-  );
+  return <> <Suspense>  {results && <ImageSearchResults results={data}/>} </Suspense>
+
+  </>;
 }
 
-const ImageDataFetcher = ({ fetchData }) => {
-  const data = fetchData();
-  const results = data?.items;
-
-  if (!results) {
-    return (
-      <div className=' flex flex-col mt-60 justify-center items-center pt-10'>
-        <h1 className='text-3xl mb-4'>No results found:(</h1>
-        <p className='text-lg'>Try searching for something else or go back to {" "}
-          <Link href="/" className="text-blue-500">homepage</Link>
-        </p>
-      </div>
-    );
-  }
-
-  return <ImageSearchResults results={results} />;
-}
-
-export default ImageSearchPage;
+export default ImageSearchPage
