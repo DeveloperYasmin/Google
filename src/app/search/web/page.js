7b7af111df3ce45f9653d24706bcbd6639391b69
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-27 12:37:47 +0530

    today

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,46 +1,9 @@
-import WebSearchResult from '@/components/WebSearchResult'
-import Link from 'next/link'
-import React, { Suspense } from 'react'
-import { useRouter } from 'next/router'
+import React from 'react'
 
-const WebSearchPage = () => {
-  const router = useRouter();
-  const { searchParams } = router.query;
-  const startIndex = searchParams?.start || "1"
-
-  const fetchData = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 7000))
-    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`)
-    if (!response.ok) {
-      throw new Error("Something went wrong")
-    }
-    const data = await response.json()
-    return data;
-  }
-
+const page = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <WebDataFetcher fetchData={fetchData} />
-    </Suspense>
-  );
+    <div>page</div>
+  )
 }
 
-const WebDataFetcher = ({ fetchData }) => {
-  const data = fetchData();
-  const results = data?.items;
-
-  if (!results) {
-    return (
-      <div className=' flex flex-col mt-60 justify-center items-center pt-10'>
-        <h1 className='text-3xl mb-4'>No results found:(</h1>
-        <p className='text-lg'>Try searching for something else or go back to {" "}
-          <Link href="/" className="text-blue-500">homepage</Link>
-        </p>
-      </div>
-    );
-  }
-
-  return <WebSearchResult results={results} />;
-}
-
-export default WebSearchPage;
+export default page
