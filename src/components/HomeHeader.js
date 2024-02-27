5e2cc4ae97210fe2c3efe60ffa489a938f38088a
A.Yasmin Banu <151750381+DeveloperYasmin@users.noreply.github.com>
2024-02-27 12:25:46 +0530

    Update HomeHeader.js

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import { CgMenuGridO } from "react-icons/cg";
 const HomeHeader = () => {
+  
   return (
     <header className='font-serif flex justify-end p-5 text-sm'>
         <div className='flex space-x-4 items-center'>
